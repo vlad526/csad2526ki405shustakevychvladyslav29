8520765fb894b4759962bb6726a54ae3f66bb9ea
vlad526 <vlad.kar765@gmail.com>
2025-10-13 21:33:07 +0300

    commit Unit Testing

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/gitpol/csad2526ki405shustakevychvladyslav29
+# Build directory: D:/gitpol/csad2526ki405shustakevychvladyslav29/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/gitpol/csad2526ki405shustakevychvladyslav29/build/unit_tests[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ build/unit_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "D:/gitpol/csad2526ki405shustakevychvladyslav29/build/unit_tests[1]_tests.cmake")
+  include("D:/gitpol/csad2526ki405shustakevychvladyslav29/build/unit_tests[1]_tests.cmake")
+else()
+  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
+endif()
