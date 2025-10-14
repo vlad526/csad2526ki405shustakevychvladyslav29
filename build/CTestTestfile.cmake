7cb75b91d2be617c9c5be7807f80d7aac52953f2
vlad526 <vlad.kar765@gmail.com>
2025-10-14 19:27:10 +0300

    Видалити папку build з репозиторію

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/gitpol/csad2526ki405shustakevychvladyslav29
-# Build directory: D:/gitpol/csad2526ki405shustakevychvladyslav29/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/gitpol/csad2526ki405shustakevychvladyslav29/build/unit_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/gitpol/csad2526ki405shustakevychvladyslav29/build/_deps/googletest-src/googletest
-# Build directory: D:/gitpol/csad2526ki405shustakevychvladyslav29/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
